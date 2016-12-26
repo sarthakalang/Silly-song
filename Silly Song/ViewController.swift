7b9039a8a045be2c0caaf1336e3211d2a73f76e1
Sarthak <sarthak@Sarthaks-MacBook-Pro.local>
2016-12-27 01:50:51 +0530

    commit

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,10 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+func shortname(_ name:String)->String{
+    var name=name
+    name.remove(at: name.startIndex)
+    return name
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+var s="fhgggvh"
+
+
+func lyricsResult(_ name:String,temp:String)->String{
+    let shortName=shortname(name)
+    let a=temp.replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    let b=a.replacingOccurrences(of: "<FULL_NAME>", with: name)
+    return(b)
+}
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate=self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +52,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func reset(_ sender: Any) {
+        nameField.text=""
+        lyricsView.text=""
+        nameField.autocapitalizationType=UITextAutocapitalizationType.words
+        nameField.returnKeyType=UIReturnKeyType.done
+        
+        
+    }
 
+    @IBAction func displayLyrics(_ sender: Any) {
+        var lyrics:String
+        let n:String=nameField.text!
+        lyrics=lyricsResult(n, temp: bananaFanaTemplate)
+        lyricsView.text=lyrics
+        
+    }
 
 }
 
